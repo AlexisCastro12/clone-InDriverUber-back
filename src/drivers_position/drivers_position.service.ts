5d0ed5c3ae581909606abf2c10266966792c0572
AlexisCastro12 <alexis1204.aacs@gmail.com>
2025-05-07 23:00:52 -0600

    Evitar sql injection en metodos para crear, insertar y obtener ubicaciones

--- src/drivers_position/drivers_position.service.ts
+++ src/drivers_position/drivers_position.service.ts
@@ -13,34 +13,45 @@ export class DriversPositionService {
 
   async create(driverPosition: createDriverPositionDto) {
     try {
+      //Se requiere crear esta variable para evitar sql injection, se inserta todo el texto de golpe en el query creado
+      const point = `SRID=4326;POINT(${driverPosition.lng} ${driverPosition.lat})`;
       //Se accede a la tabla mediante el schema udemy_delivery.drivers_position, esto puede cambiar en produccion
-      const data: any[] = await this.driversPositionRepository.query(`
+      const data: any[] = await this.driversPositionRepository.query(
+        `
         SELECT
           *
         FROM
           udemy_delivery.drivers_position
         WHERE
-          id_driver = ${driverPosition.id_driver}
-        `);
+          id_driver = $1
+        `,
+        [driverPosition.id_driver],
+      );
       //Se tipó data con any[] para que al aplicarle .length no marque error de sintaxis
       if (data.length <= 0) {
-        await this.driversPositionRepository.query(`
+        await this.driversPositionRepository.query(
+          `
             INSERT INTO
               udemy_delivery.drivers_position(id_driver, location)
             VALUES(
-            ${driverPosition.id_driver},
-            ST_GeogFromText('SRID=4326;POINT(${driverPosition.lng} ${driverPosition.lat})')
+            $1,
+            ST_GeogFromText($2)
             )
-            `);
+            `,
+          [driverPosition.id_driver, point],
+        );
       } else {
-        await this.driversPositionRepository.query(`
+        await this.driversPositionRepository.query(
+          `
           UPDATE
             udemy_delivery.drivers_position
           SET
-            location = ST_GeogFromText('SRID=4326;POINT(${driverPosition.lng} ${driverPosition.lat})')
+            location = ST_GeogFromText($1)
           WHERE
-          id_driver = ${driverPosition.id_driver}
-          `);
+          id_driver = $2
+          `,
+          [point, driverPosition.id_driver],
+        );
       }
       return true;
     } catch (error) {
@@ -50,18 +61,23 @@ export class DriversPositionService {
   }
 
   async getNearbyDrivers(client_lat: number, client_lng: number) {
-    const driversPosition = await this.driversPositionRepository.query(`
+    //Se requiere crear esta variable para evitar sql injection, se inserta todo el texto de golpe en el query creado
+    const point = `SRID=4326;POINT(${client_lng} ${client_lat})`;
+    const driversPosition = await this.driversPositionRepository.query(
+      `
       SELECT
         id_driver,
         ST_Y(location::geometry) AS locationlat,
         ST_X(location::geometry) AS locationlng,
-        ST_Distance(location::geography, ST_GeogFromText('SRID=4326; POINT(${client_lng} ${client_lat})')) AS distance
+        ST_Distance(location::geography, ST_GeogFromText($1)) AS distance
       FROM
         udemy_delivery.drivers_position
-      WHERE ST_DWithin(location::geography, ST_GeogFromText('SRID=4326; POINT(${client_lng} ${client_lat})') , 500)
+      WHERE ST_DWithin(location::geography, ST_GeogFromText($1) , 500)
       ORDER BY
         distance ASC;
-      `);
+      `,
+      [point],
+    );
     return driversPosition;
   }
 
