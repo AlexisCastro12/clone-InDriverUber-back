93c3fd701166c6f1c932f9841050164b0b31e45a
AlexisCastro12 <alexis1204.aacs@gmail.com>
2025-05-07 20:54:06 -0600

    Insertar nuevo registro en tabla drivers_position

--- src/drivers_position/drivers_position.controller.ts
+++ src/drivers_position/drivers_position.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { DriversPositionService } from './drivers_position.service';
+import { createDriverPositionDto } from './dto/create_driver_position.dto';
 
 @Controller('drivers-position')
-export class DriversPositionController {}
+export class DriversPositionController {
+  constructor(private driversPositionService: DriversPositionService) {}
+
+  @Post()
+  create(@Body() driverPosition: createDriverPositionDto) {
+    return this.driversPositionService.create(driverPosition);
+  }
+}
--- src/drivers_position/drivers_position.service.ts
+++ src/drivers_position/drivers_position.service.ts
@@ -1,4 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { DriversPosition } from './drivers_position.entity';
+import { Repository } from 'typeorm';
+import { createDriverPositionDto } from './dto/create_driver_position.dto';
 
 @Injectable()
-export class DriversPositionService {}
+export class DriversPositionService {
+  constructor(
+    @InjectRepository(DriversPosition)
+    private driversPositionRepository: Repository<DriversPosition>,
+  ) {}
+
+  async create(driverPosition: createDriverPositionDto) {
+    try {
+      //Se accede a la tabla mediante el schema udemy_delivery.drivers_position, esto puede cambiar en produccion
+      await this.driversPositionRepository.query(`
+        INSERT INTO
+          udemy_delivery.drivers_position(id_driver, location)
+        VALUES(
+        ${driverPosition.id_driver},
+        ST_GeogFromText('SRID=4326;POINT(${driverPosition.lng} ${driverPosition.lat})')
+        )
+        `);
+      return true;
+    } catch (error) {
+      console.log('Error al registrar la ubicacion del conductor:\n', error);
+      return false;
+    }
+  }
+}
