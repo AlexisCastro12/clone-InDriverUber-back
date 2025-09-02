9e3ba299d499309ba6e910ab44e24b8b4384e6ca
AlexisCastro12 <alexis1204.aacs@gmail.com>
2025-09-02 16:22:28 -0600

    Eliminar ConfigModule (codigo redundante), ya es global (AppModule)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -10,7 +10,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule,
+    // ConfigModule, //ya no se necesita porque ya se configuro globalmente en appModule
     TypeOrmModule.forFeature([User, Rol]),
     JwtModule.registerAsync({
       imports: [ConfigModule], // importa ConfigModule aquí también
